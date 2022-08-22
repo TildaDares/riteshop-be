00a6747c3c412ec5e4d14f166497d5abc5ceeedc
tildaudufo <mathilda.udufo@deimos.co.za>
2022-08-22 09:38:22 +0100

    update package.json scripts

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,12 +3,18 @@ import { createClient } from 'redis';
 export type RedisClientType = ReturnType<typeof createClient>
 
 let redisClient: RedisClientType;
-async function redisConfig () {
-  redisClient = createClient();
+async function redisConfig() {
+  redisClient = createClient({
+    socket: {
+      host: process.env.REDIS_HOST || '127.0.0.1',
+      port: 6379
+    }
+  });
 
   redisClient.on("error", (error) => {
     console.log(error);
   });
+
   redisClient.on("connect", () => {
     console.log("Redis connected!");
   });
